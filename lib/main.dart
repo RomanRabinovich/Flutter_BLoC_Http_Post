7131d56bec9048d8008e3c30da60c80931c5ad12
RomanRabinovich <roman.kliakhin@gmail.com>
2022-09-03 20:36:21 +0300

    Not finished yet

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
